27f449c1cc68219621f22a5974deab2cb4a4b506
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2020-10-11 23:52:27 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/ProgrammerZamanNow/go-say-hello
+module github.com/ProgrammerZamanNow/go-say-hello/v2
 
 go 1.15
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello World"
+func SayHello(name string) string {
+	return "Hello " + name
 }
